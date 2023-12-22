3911cb7344110edfefac787ca2cdff89d9142deb
Fritz Schubert <fritzschubert21@outlook.de>
2023-12-22 21:32:27 +0100

    Add IntroAnimation component and Song interface

---
+++ src/lib/Song.ts
@@ -0,0 +1,9 @@
+// Song.ts
+export interface Song {
+    last_checked: string;
+    last_updated: string;
+    spotify_id: string;
+    time_range: string;
+    track_name: string;
+    _id: string;
+}
