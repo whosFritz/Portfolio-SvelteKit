6ae1aa949716e949dbed5586b368551851d1ad3d
whosfritz <fritzschubert21@outlook.de>
2024-04-08 17:26:58 +0200

    refactored

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -1,20 +1,18 @@
 <script lang="ts">
-  import SingelProject from "./ProjectCard.svelte";
-  import Divider from "./Divider.svelte";
-  import { projects } from "./projects";
+	import SingelProject from './ProjectCard.svelte';
+	import Divider from './Divider.svelte';
+	import { projects } from './projects';
 </script>
 
 <section id="projects-section" class="my-12">
-  <p class="text-center text-3xl font-bold my-8">
-    <span> Projects </span>
-    <span class="glowing" style="--intensity: 4px; --display-color: pink;">
-      🚀
-    </span>
-  </p>
-  {#each projects as project, i}
-    <SingelProject {project} />
-    {#if i < projects.length - 1}
-      <Divider />
-    {/if}
-  {/each}
+	<p class="text-center text-3xl font-bold my-8">
+		<span> Projects </span>
+		<span class="glowing" style="--intensity: 4px; --display-color: pink;"> 🚀 </span>
+	</p>
+	{#each projects as project, i}
+		<SingelProject {project} />
+		{#if i < projects.length - 1}
+			<Divider />
+		{/if}
+	{/each}
 </section>
