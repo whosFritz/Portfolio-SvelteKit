13b42595a6ede1cce39d5b197053c91b0cb0efab
whosfritz <fritzschubert21@outlook.de>
2024-04-08 17:27:08 +0200

    added grow on hover to project cards

--- src/lib/ProjectCard.svelte
+++ src/lib/ProjectCard.svelte
@@ -1,46 +1,49 @@
 <script lang="ts">
-  import type { Project } from "./types";
-  export let project: Project;
+	import type { Project } from './types';
+	export let project: Project;
 </script>
 
 <div
-  class="flex flex-col md:flex-row items-center my-4 rounded-lg p-4 shadow-2xl backshadow"
+	class="flex flex-col md:flex-row items-center my-4 rounded-lg p-4 shadow-2xl backshadow growOnHover"
 >
-  <div class="md:w-3/5">
-    <a href={project.projectLink} target="_blank" class={project.displayColor}>
-      <p
-        color={project.displayColor}
-        class="flex text-2xl glowing"
-        style="--intensity: 2px; --display-color: {project.displayColor};"
-      >
-        {project.heading}
-      </p>
-    </a>
+	<div class="md:w-3/5">
+		<a href={project.projectLink} target="_blank" class={project.displayColor}>
+			<p
+				color={project.displayColor}
+				class="flex text-2xl glowing"
+				style="--intensity: 2px; --display-color: {project.displayColor};"
+			>
+				{project.heading}
+			</p>
+		</a>
 
-    {#each project.descriptions as description}
-      <p class="mt-3 font-semibold">{description}</p>
-    {/each}
-  </div>
+		{#each project.descriptions as description}
+			<p class="mt-3 font-semibold">{description}</p>
+		{/each}
+	</div>
 
-  <div class="w-44 mx-auto my-8">
-    <a href={project.projectLink} target="_blank">
-      <img
-        src={project.imageURL}
-        alt={project.heading}
-        class="rounded-3xl shadow-lg"
-      />
-    </a>
-  </div>
+	<div class="w-44 mx-auto my-8">
+		<a href={project.projectLink} target="_blank">
+			<img src={project.imageURL} alt={project.heading} class="rounded-3xl shadow-lg" />
+		</a>
+	</div>
 </div>
 
 <style>
-  .backshadow:hover {
-    --tw-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25);
-    --tw-shadow-colored: 0 25px 50px -12px var(--tw-shadow-color);
-    box-shadow:
-      var(--tw-ring-offset-shadow, 0 0 #0000),
-      var(--tw-ring-shadow, 0 0 #0000),
-      var(--tw-shadow),
-      0 0 10px rgba(0, 0, 0, 0.2);
-  }
+	.backshadow:hover {
+		--tw-shadow: 0 25px 50px -12px rgb(0 0 0 / 0.25);
+		--tw-shadow-colored: 0 25px 50px -12px var(--tw-shadow-color);
+		box-shadow:
+			var(--tw-ring-offset-shadow, 0 0 #0000),
+			var(--tw-ring-shadow, 0 0 #0000),
+			var(--tw-shadow),
+			0 0 10px rgba(0, 0, 0, 0.2);
+	}
+	.growOnHover {
+		transition: all 0.2s ease-in-out;
+	}
+
+	.growOnHover:hover {
+		transform: scale(1.05);
+	}
 </style>
