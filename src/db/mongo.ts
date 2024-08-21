7cf4768ab0eb25f38ad82ca577f34d7e3da10d2f
whosfritz <fritzschubert21@outlook.de>
2024-08-21 10:23:47 +0200

    testing build pipeline

--- src/db/mongo.ts
+++ src/db/mongo.ts
@@ -1,7 +1,6 @@
 import { MongoClient } from "mongodb";
 
 const MONGODB_URL = "mongodb://mongo_container:27017/favSongswhosfritz";
-
 console.log("Connecting to mongo at " + MONGODB_URL);
 const client = new MongoClient(MONGODB_URL);
 export function start_mongo() {
