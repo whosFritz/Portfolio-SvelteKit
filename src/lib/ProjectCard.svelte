d68ed8ec4443b5884c157d14e04097384ed61938
whosfritz <fritzschubert21@outlook.de>
2024-09-09 09:31:52 +0200

    observing just once

--- src/lib/ProjectCard.svelte
+++ src/lib/ProjectCard.svelte
@@ -1,33 +1,48 @@
 <script lang="ts">
 	import type { Project } from './types';
 	export let project: Project;
+
+	import IntersectionObserver from 'svelte-intersection-observer';
+	let elementOnce: HTMLDivElement;
+	let intersectOnce: boolean;
+	let threshold = 0.5;
 </script>
 
-<div
-	class="flex flex-col md:flex-row items-center my-4 rounded-3xl p-4 shadow-2xl backshadow growOnHover border border-slate-500"
+<IntersectionObserver
+	once
+	element={elementOnce}
+	bind:intersecting={intersectOnce}
+	rootMargin="0px 0px -100px 0px"
+	{threshold}
 >
-	<div class="md:w-3/5">
-		<a href={project.projectLink} target="_blank" class={project.displayColor}>
-			<p
-				color={project.displayColor}
-				class="flex text-2xl "
-				style="color: {project.displayColor};"
-			>
-				{project.heading}
-			</p>
-		</a>
+	<div
+		bind:this={elementOnce}
+		class:intersecting={intersectOnce}
+		class="fade-in flex flex-col md:flex-row items-center my-4 rounded-3xl p-4 shadow-2xl backshadow growOnHover border border-slate-500"
+	>
+		<div class="md:w-3/5">
+			<a href={project.projectLink} target="_blank" class={project.displayColor}>
+				<p
+					color={project.displayColor}
+					class="flex text-2xl"
+					style="color: {project.displayColor};"
+				>
+					{project.heading}
+				</p>
+			</a>
 
-		{#each project.descriptions as description}
-			<p class="mt-3 leading-8 font-semibold">{description}</p>
-		{/each}
-	</div>
+			{#each project.descriptions as description}
+				<p class="mt-3 leading-8 font-semibold">{description}</p>
+			{/each}
+		</div>
 
-	<div class="w-44 mx-auto my-8">
-		<a href={project.projectLink} target="_blank">
-			<img src={project.imageURL} alt={project.heading} class="rounded-3xl shadow-lg" />
-		</a>
+		<div class="w-44 mx-auto my-8">
+			<a href={project.projectLink} target="_blank">
+				<img src={project.imageURL} alt={project.heading} class="rounded-3xl shadow-lg" />
+			</a>
+		</div>
 	</div>
-</div>
+</IntersectionObserver>
 
 <style>
 	.backshadow:hover {
